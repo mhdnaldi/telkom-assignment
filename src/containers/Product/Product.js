05473d049b6bee76af227aa696fe30a9c3ab8206
mhdnaldi <mhd.naldi@yahoo.com>
2021-02-19 10:50:13 +0700

    add product page styling

--- src/containers/Product/Product.js
+++ src/containers/Product/Product.js
@@ -17,7 +17,37 @@ const Product = (props) => {
             <div className='small'></div>
           </div>
         </div>
-        <div className='product-details'></div>
+        <div className='product-details'>
+          <h3>TITLE</h3>
+          <div className='product-price product-info'>
+            <p>HARGA</p>
+            <strong>Rp. 200.000</strong>
+          </div>
+          <div className='product-colors product-info'>
+            <p>WARNA</p>
+            <div className='product-variant'>
+              <p>Pilih variant</p>
+              <div className='variants'>
+                <div className='variant-colors'>Hijau</div>
+                <div className='variant-colors'>Abu-abu</div>
+                <div className='variant-colors'>Coklat</div>
+                <div className='variant-colors'>Putih</div>
+                <div className='variant-colors'>Biru</div>
+              </div>
+            </div>
+          </div>
+          <div className='product-size product-info'>
+            <p>UKURAN</p>
+            <div className='product-variant'>
+              <p>Pilih variant</p>
+              <div className='variants'>
+                <div>M</div>
+                <div>L</div>
+                <div>XL</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className='checkout-footer'></div>
     </div>
